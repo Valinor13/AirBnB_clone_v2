7369e99f0532e1913b0ee228533d7627f79110a2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-19 23:39:04 +0000

    Changing syntax

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
     """ python (is cool) """
     return "Python {}".format(text.replace('_', ' '))
