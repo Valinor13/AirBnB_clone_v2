3a89eb25b30dfd87ae2a775f06f87a2909af236f
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-19 23:19:14 +0000

    Adding documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" This module is the flask backend to communicate with the web site"""
 
 from flask import Flask
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
+    """ index is the html home page """
     return "Hello HBNB!"
 
 if __name__== '__main__':
