c2d4fe615f37aa0da2f4b8608aa5e07c53a9866e
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-19 23:32:05 +0000

    Adding choose your route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module is the flask backend to communicate with the web site"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ index is the html home page """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ the hbnb web page """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ define your url """
+    return "C {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
