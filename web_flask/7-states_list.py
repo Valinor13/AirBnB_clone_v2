b417e27eaacd1a05b001ce05b7b146a8e93f3bd7
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-20 02:42:28 +0000

    Fixing pepe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,8 +15,8 @@ def states_list():
 
     for key, value in storage.all(State).items():
         slist.append((value.name, value.id))
-    slist.sort(key = lambda x: x[0])
-    return  render_template("7-states_list.html", slist=slist)
+    slist.sort(key=lambda x: x[0])
+    return render_template("7-states_list.html", slist=slist)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
